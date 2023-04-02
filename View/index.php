bc30c41396e8a13122e7f266bffbd924eee2e2d1
fabs1902 <36040836+fabs1902@users.noreply.github.com>
2023-04-02 15:51:42 -0600

    cambios 2.0

--- View/index.php
+++ View/index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-<<<<<<< Updated upstream
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,32 +9,15 @@
       include 'cabecera.php';            
     ?>
     <title>Home | La Cuponera</title>
-=======
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <?php
-   include 'cabecera.php'
-   ?>
-   <title>Home | La Cuponera</title>
->>>>>>> Stashed changes
 </head>
 
 <body style="background-color: beige">
    <?php
-<<<<<<< Updated upstream
       include 'menu.php';      
    ?>
    <main>
 		<div class="container-fluid d-none d-sm-block" style="padding: 0">         
          <img src="/LaCuponera/View/assets/img/imgPrincipal4.jpg" class="img-fluid imgPrincipal" alt="Imagen principal">
-=======
-   include 'menu.php';
-   ?>
-   <main>
-      <div class="container-fluid d-none d-sm-block" style="padding: 0">
-         <img src="assets/img/imgPrincipal4.jpg" class="img-fluid imgPrincipal" alt="Imagen principal">
->>>>>>> Stashed changes
       </div>
       <div class="container-fluid" style="padding:10px">
          <div class="row">
@@ -47,65 +29,19 @@
             </div>
          </div>
          <div class="linea"></div>
-<<<<<<< Updated upstream
       </div>     
       
       <div class="container-fluid">        
-=======
-      </div>
-
-      <div class="container-fluid">
->>>>>>> Stashed changes
          <div class="row text-center">
             <h3 style="color:#7D2972">Conoce las ofertas...</h3>
          </div>
          <div class="row">
-<<<<<<< Updated upstream
-            <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-               <div class="card">
-                  <img src="/LaCuponera/View/assets/img/polloCampero.png" class="card-img-top" alt="...">
-                  <div class="card-body">
-                     <h5 class="card-title">Pollo Campero</h5>
-                     <p class="card-text">Conoce las promociones de Pollo Campero</p>
-                     <div class = "d-flex justify-content-center">
-                        <a href="offersCompany.php" class="btn btn-primary" style="background-color: #7D2972; color : white">Ver promociones</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-               <div class="card">
-                  <img src="/LaCuponera/View/assets/img/polloCampero.png" class="card-img-top" alt="...">
-                  <div class="card-body">
-                     <h5 class="card-title">Pollo Campero</h5>
-                     <p class="card-text">Conoce las promociones de Pollo Campero</p>
-                     <div class = "d-flex justify-content-center">
-                        <a href="offersCompany.php" class="btn btn-primary" style="background-color: #7D2972; color : white">Ver promociones</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-               <div class="card">
-                  <img src="/LaCuponera/View/assets/img/polloCampero.png" class="card-img-top" alt="...">
-                  <div class="card-body">
-                     <h5 class="card-title">Pollo Campero</h5>
-                     <p class="card-text">Conoce las promociones de Pollo Campero</p>
-                     <div class = "d-flex justify-content-center">
-                        <a href="offersCompany.php" class="btn btn-bd-primary" style="background-color: #7D2972; color : white">Ver promociones</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>  
-=======
             <form action="#" method="POST">
                <div class="d-flex justify-content-center">
                   <a type="submit" class="btn btn-primary text-center" style="background-color: #7D2972; color : white; height: 50px; width:200px; font-size:20px; ">Ver promociones</a>
                </div>
             </form>
          </div>
->>>>>>> Stashed changes
       </div>
    </main>
    <?php
